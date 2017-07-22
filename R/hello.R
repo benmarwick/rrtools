600b32bb7d2e8dfc9d49939c8e06ada0aa17824f
Ben Marwick <benmarwick@gmail.com>
2017-07-22 12:19:18 +0200

    use variables in travis

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,30 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' @rdname infrastructure
+#' @section \code{use_travis}:
+#' Add basic travis template to a package. Also adds \code{.travis.yml} to
+#' \code{.Rbuildignore} so it isn't included in the built package.
+#' @param browse open a browser window to enable Travis builds for the package
+#' automatically.
+#' @import devtools
+#' @export
+#' @aliases add_travis
+use_travis <- function(pkg = ".", browse = interactive()) {
+  pkg <- as.package(pkg)
 
-hello <- function() {
-  print("Hello, world!")
+  use_template("travis.yml", ".travis.yml", ignore = TRUE, pkg = pkg)
+
+  gh <- github_info(pkg$path)
+  travis_url <- file.path("https://travis-ci.org", gh$fullname)
+
+  message("Next: \n",
+          " * Add a travis shield to your README.md:\n",
+          "[![Travis-CI Build Status]",
+          "(https://travis-ci.org/", gh$fullname, ".svg?branch=master)]",
+          "(https://travis-ci.org/", gh$fullname, ")\n",
+          " * Turn on travis for your repo at ", travis_url, "\n"
+  )
+  if (browse) {
+    utils::browseURL(travis_url)
+  }
+
+  invisible(TRUE)
 }
