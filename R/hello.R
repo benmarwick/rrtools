988569937487c0d7e02f1450855769f36db2fffb
Ben Marwick <benmarwick@gmail.com>
2017-07-22 13:15:23 +0200

    add custom use_travis function

--- R/hello.R
+++ R/hello.R
@@ -8,22 +8,60 @@
 #' @export
 #' @aliases add_travis
 use_travis <- function(pkg = ".", browse = interactive()) {
-  pkg <- as.package(pkg)
+  pkg <- devtools::as.package(pkg)
 
-  use_template("travis.yml", ".travis.yml", ignore = TRUE, pkg = pkg)
-
-  gh <- github_info(pkg$path)
+  gh <- devtools:::github_info(pkg$path)
   travis_url <- file.path("https://travis-ci.org", gh$fullname)
 
+  rrtools:::use_template("travis.yml",
+                         ".travis.yml",
+                         ignore = TRUE,
+                         pkg = pkg,
+                         data = gh)
+
+
+
   message("Next: \n",
-          " * Add a travis shield to your README.md:\n",
+          " * Add a travis shield to your README.Rmd:\n",
           "[![Travis-CI Build Status]",
           "(https://travis-ci.org/", gh$fullname, ".svg?branch=master)]",
           "(https://travis-ci.org/", gh$fullname, ")\n",
-          " * Turn on travis for your repo at ", travis_url, "\n"
+          " * Turn on travis for your repo at ", travis_url, "\n",
+          " * At travis, add your environment variables: DOCKER_EMAIL, DOCKER_USER, DOCKER_PASS to enable pushing to the Docker Hub"
   )
   if (browse) {
     utils::browseURL(travis_url)
+  }
+
+  invisible(TRUE)
+}
+
+
+
+use_template <- function(template, save_as = template, data = list(),
+                         ignore = FALSE, open = FALSE, pkg = ".") {
+  pkg <- devtools::as.package(pkg)
+
+  path <- file.path(pkg$path, save_as)
+  if (!devtools:::can_overwrite(path)) {
+    stop("`", save_as, "` already exists.", call. = FALSE)
+  }
+
+  template_path <- system.file("templates", template, package = "rrtools",
+                               mustWork = TRUE)
+  template_out <- whisker::whisker.render(readLines(template_path), data)
+
+  message("* Creating `", save_as, "` from template.")
+  writeLines(template_out, path)
+
+  if (ignore) {
+    message("* Adding `", save_as, "` to `.Rbuildignore`.")
+    devtools::use_build_ignore(save_as, pkg = pkg)
+  }
+
+  if (open) {
+    message("* Modify `", save_as, "`.")
+    devtools::open_in_rstudio(path)
   }
 
   invisible(TRUE)
