2f29122566c2c6949aad1a1d2961a9f5b61e0867
Ben Marwick <benmarwick@gmail.com>
2017-09-10 20:39:20 -0700

    open_data -> data_in_git, closes #39

--- R/hello.R
+++ R/hello.R
@@ -248,7 +248,7 @@ use_analysis <- function(pkg = ".", location = "top_level", template = 'paper.Rm
           " * Add reference details to the references.bib in analysis/paper/", "\n",
           " * For adding captions & cross-referenceing in an Rmd, see https://bookdown.org/yihui/bookdown/ ", "\n",
           " * For adding citations & reference lists in an Rmd, see http://rmarkdown.rstudio.com/authoring_bibliographies_and_citations.html ", "\n",
-          ifelse(!open_data,
+          ifelse(!data_in_git,
           " * Your data files are NOT tracked by Git and will not be pushed to GitHub", ""))
 
   open_in_rstudio(file.path(pkg$path, location, "paper/paper.Rmd"))
