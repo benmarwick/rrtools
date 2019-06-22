2be8d0c4e0d1d2ac6f3c59c8daa400aa86b53c5d
ntrlshrp <3910978-ntrlshrp@users.noreply.gitlab.com>
2019-06-22 01:29:50 -0400

    Enabled `rrtools::use_analysis(location = "vignettes")` to set up compendium
    This function failed to set up the compendium because a
    `usethis::ui_done()` was malformed (a typo turned the
    required 1 argument into 4 arguments).
    
    Once this was fixed, it turned out that some
    `writeLines()` functions were not pointing to the
    correct path and thus failed to create `/paper.Rmd`.
    
    Once this was fixed,
    `tests/testthat/test_use_analysis.R` was extended to
    test that `location = "inst"` and `location =
    "vignettes"` create the proper analysis sub-directories
    _and_ that `paper/` contains `paper.Rmd` and
    `references.bib`.
    
    Although `devtools::check()` completed with 0 errors,
    warnings, or notes, `rrtools::use_analysis(location =
    "vignettes")` still failed in the field. This was again
    a path error (this time in `open_in_rstudio()`). This
    was rectified and new code was added so that RStudio
    opens `paper.Rmd` for the user for all three locations,
    i.e., `"top_level"`, `"inst"`, and `"vignettes"`.

--- tests/testthat/test_use_analysis.R
+++ tests/testthat/test_use_analysis.R
@@ -1,6 +1,6 @@
 context("use_analysis()")
 
-#### run function ####
+#### run function default (analysis/) ####
 
 suppressMessages(
   rrtools::use_analysis(pkg = package_path)
@@ -14,6 +14,46 @@ test_that("use_analysis generates the template directories and files", {
                c("data", "figures",  "paper", "templates"))
 
   expect_equal(list.files(file.path(package_path, 'analysis', 'paper')),
+               c("paper.Rmd", "references.bib"))
+
+})
+
+
+
+#### run function inst/ ####
+
+suppressMessages(
+  rrtools::use_analysis(pkg = package_path, location = "inst")
+)
+
+#### check results ####
+
+test_that("use_analysis(location = 'inst') generates the template directories and files", {
+
+  expect_equal(list.files(file.path(package_path, 'inst')),
+               c("data", "figures",  "paper", "templates"))
+
+  expect_equal(list.files(file.path(package_path, 'inst', 'paper')),
+               c("paper.Rmd", "references.bib"))
+
+})
+
+
+
+#### run function vignettes/ ####
+
+suppressMessages(
+  rrtools::use_analysis(pkg = package_path, location = "vignettes")
+)
+
+#### check results ####
+
+test_that("use_analysis(location = 'vignettes') generates the template directories and files", {
+
+  expect_equal(list.files(file.path(package_path, 'vignettes')),
+               c("data", "figures",  "paper", "templates"))
+
+  expect_equal(list.files(file.path(package_path, 'vignettes', 'paper')),
                c("paper.Rmd", "references.bib"))
 
 })
