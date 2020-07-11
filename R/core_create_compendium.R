3ba7334d52709597d44586f0c374d6f9b0a45eae
Ben Marwick <benmarwick@hotmail.com>
2020-07-10 23:43:48 -0700

    make create_comp behave same as use_comp wrt path

--- R/core_create_compendium.R
+++ R/core_create_compendium.R
@@ -3,22 +3,23 @@
 #'
 #' @description In one step, this will create an R package in an empty, git initialized directory, attach the MIT license to it, add the rrtools' README to it, create the 'analysis' directory structure, and populate it with an R Markdown file and bib file. This function will not create a GitHub repository for the compendium, a Dockerfile, a Travis config file, or any package tests. Those require some interaction outside of R and are left to the user.
 #'
-#' @param pkgname path to an empty, git initialized directory. The last component of the path will be used as the package name
+#' @param pkgname path to an empty, git initialized directory. The last component of the path will be used as the package name. Default is the current directory name.
 #' @param data_in_git should git track the files in the data directory? Default is TRUE
 #' @param simple if TRUE, the default, the R/ directory is not created, because it's not necessary for many if not most research repositories
 #'
 #' @importFrom usethis use_mit_license use_git
 #' @export
 
-create_compendium <- function(pkgname,
+create_compendium <- function(pkgname = getwd(),
                               data_in_git = TRUE,
                               simple = TRUE) {
+
 
   # create new project
   rrtools::use_compendium(pkgname, simple = simple)
 
   # move us into the new project
-  setwd(pkgname)
+  # setwd(pkgname)
 
   # initialize the new project with useful features
   usethis::use_mit_license(name = get_git_config("user.name", global = TRUE))
